f78127d07a2363b7243a3e5d36560429a4436457
razon1494 <razon1494@gmail.com>
2021-10-28 18:43:11 +0600

    first commit

---
+++ index.js
@@ -0,0 +1,67 @@
+const express=require('express');
+const {MongoClient}=require('mongodb');
+const cors=require('cors');
+require('dotenv').config();
+const ObjectId=require('mongodb').ObjectId;
+const app=express();
+const port=5000;
+app.use(cors());
+app.use(express.json());
+
+const uri=`mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.yug9g.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+
+console.log(uri);
+
+const client=new MongoClient(uri, {useNewUrlParser: true, useUnifiedTopology: true});
+
+async function run() {
+  try {
+      await client.connect();
+      console.log('connected to databse from club mor');
+      const database=client.db("carMechanic");
+      const servicesCollection=database.collection("services");
+    //GET API
+      app.get('/services', async (req, res) => {
+          const cursor=servicesCollection.find({});
+          const services=await cursor.toArray();
+          res.send(services);
+      })
+      //GET Single Service
+      app.get('/services/:id', async (req, res) => {
+          const id=req.params.id;
+          console.log('got specific id', id);
+          const query={_id: ObjectId(id)};
+          const service=await servicesCollection.findOne(query);
+          res.json(service);
+
+
+      })
+
+    // POST API
+      app.post('/services', async (req, res) => {
+          const service=req.body;
+          console.log('result hitted the post api', service);
+          const result=await servicesCollection.insertOne(service);
+          console.log(result);
+          res.send(result);
+      });
+      //DELETE API
+      app.delete('/services/:id', async (req, res) => {
+          const id=req.params.id;
+          const query={_id: ObjectId(id)};
+          const result=await servicesCollection.deleteOne(query);
+          res.json(result);
+      })
+  } finally {
+    // await client.close();
+  }
+}
+run().catch(console.dir);
+
+app.get('/', (req, res) => {
+    res.send('running genius server');
+});
+
+app.listen(port, () => {
+    console.log('running my car server on port', port);
+})
